b6fe42641824fbf033875fc6d2bdb893f14b958e
thach vu <thachvu1127@gmail.com>
2024-10-02 11:40:06 -0700

    add search function to weather app

--- index.js
+++ index.js
@@ -7,16 +7,41 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded( {extended: true }));
 
 app.get("/", async (req, res) => {
     try {
-        const response = await axios.get("http://api.weatherapi.com/v1/current.json?key=31d4fd31058e4e319d2212521240110&q=London&aqi=no");
-        const result = response.data;
         res.render("index.ejs");
     } catch (error) {
         console.log("Failed to make request:", error.message);
     }
 })
+
+app.post("/submit", async (req, res) => {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const d = new Date();
+    
+
+    
+    try {
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=31d4fd31058e4e319d2212521240110&q=${req.body.location}&aqi=no`);
+        const data = response.data;
+        const splitDate = data.location.localtime.split(" ")[0];
+        const splitTime = data.location.localtime.split(" ")[1];
+        const d = new Date(splitDate);
+        const curentDay = days[d.getUTCDay()]
+
+        res.render("index.ejs", {
+            temp: `${data.current.temp_f}° F`,
+            city: data.location.name,
+            condition: data.current.condition.text,
+            date: `${splitDate} ${curentDay} ${splitTime}`
+        });
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+    }
+
+});
 
 app.listen(port, () => {
     console.log("Listening on port " + port);
